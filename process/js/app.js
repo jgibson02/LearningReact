162d6b25bab15b80c936bf59ab064a8ade8bf417
John Gibson <jgibson02@users.noreply.github.com>
2017-07-14 19:01:52 -0400

    Initial upload

---
+++ process/js/app.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var MainInterface = React.createClass({
+    render: function() {
+        return (
+            <div className="interface">
+                <h1>Pet Appointments</h1>
+                <ul>
+                    <li>Buffy 3:30 PM</li>
+                    <li>Spot 8:30</li>
+                    <li>Goldie 3:50</li>
+                </ul>
+            </div>
+        )
+    } //render
+}); //MainInterface
+
+ReactDOM.render(
+    <MainInterface />,
+    document.getElementById('petAppointments')
+);
